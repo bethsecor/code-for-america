8dae302af6e3fe718c0b1dff4028805d99117f36
Beth Secor <elizabeth.a.secor@gmail.com>
2016-07-06 17:59:56 -0600

    Add data and R solution to violations data

---
+++ violations.R
@@ -0,0 +1,16 @@
+violations.data <- read.csv("/Users/elizabethsecor/Documents/code-for-america/Violations-2012.csv", header=TRUE)
+violations.data$violation_date <- as.Date(violations.data$violation_date)
+
+results <- matrix(NA, 9, 4)
+colnames(results) <- c("Category", "Number of Violations", "Earliest Violation Date", "Latest Violation Date")
+
+category.summary <- table(violations.data$violation_category)
+results[,1] <- names(category.summary)
+results[,2] <- category.summary
+
+minimums <- c(by(violations.data$violation_date, violations.data$violation_category, min))
+results[,3] <- as.character(as.Date(as.numeric(minimums), origin = "1970-01-01"))
+
+maximums <- by(violations.data$violation_date, violations.data$violation_category, max)
+results[,4] <- as.character(as.Date(as.numeric(maximums), origin = "1970-01-01"))
+results
